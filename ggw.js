35714fa980a20c09d3fb8eec1def3d476995b933
luminafaith <leah.young7061@gmail.com>
2024-01-10 23:32:15 -0500

    Initialize js

---
+++ ggw.js
@@ -0,0 +1,3 @@
+// several options here for how to go about changing the image based on the value of select
+// https://stackoverflow.com/questions/33967140/change-html-according-to-select
+// try listener? and then can attempt to do jquery later once everything is working
